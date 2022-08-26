0040103d4f7c8a0baefa01b1ef968a9887c0164e
CodeXzzz <codexzzz@outlook.com>
2022-08-26 08:53:48 +0800

    feat:add like model

---
+++ model/like.go
@@ -0,0 +1,11 @@
+package model
+
+import "gorm.io/gorm"
+
+type Like struct {
+	gorm.Model
+	TypeID int  `json:"typeId"` //对应文章或评论（回复）的ID
+	Type   bool `json:"type"`   //0是作品（文章）点赞，1是评论（回复）点赞
+	Uid    int  `json:"uid"`    //与MUser表中的Uid一致
+	Status int  `json:"status"` //
+}
