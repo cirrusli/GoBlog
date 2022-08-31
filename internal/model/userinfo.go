0dcde0e496643b8a8ac302562263b4abf7a181e5
CodeXzzz <codexzzz@outlook.com>
2022-08-31 11:05:36 +0800

    update userinfo.go

--- internal/model/userinfo.go
+++ internal/model/userinfo.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 // MUser 用户信息
 type MUser struct {
+	//todo 结构体内存对齐
 	gorm.Model
 	Uid       int    `json:"uid" gorm:"primaryKey"` //EncodeID
 	UserName  string `json:"username"`
@@ -11,6 +12,7 @@ type MUser struct {
 	AvatarUrl string `json:"avatarUrl" gorm:"default:https://image-1302243118.cos.ap-beijing.myqcloud.com/public/img/BLUE_GLASSES_GOPHER-1616727843503.png"` //用户头像路径
 	//Age       byte  `json:"age" gorm:"default 18"`    //当该字段在数据库中无默认值时不会生效
 	//Gender    byte  `json:"gender"`                   //1 is male,0 is female
+	//PhoneNumber string `json:"phone_number"` //手机号
 }
 
 // LoginRes 登录时携带token
