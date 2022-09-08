14f8033a6696b6cac7ad327940003384b4c13271
CodeXzzz <codexzzz@outlook.com>
2022-09-08 20:35:55 +0800

    daily develop

--- internal/service/likeService.go
+++ internal/service/likeService.go
@@ -3,12 +3,14 @@ package service
 import (
 	"SummerProject/internal/dao"
 	"SummerProject/internal/model"
+	"errors"
 )
 
 func LikeAction(likeStructure *model.Like) (err error) {
+	//todo add redis cache
 	err = dao.LikeAction(likeStructure)
 	if err != nil {
-		return err
+		return errors.New("点赞失败")
 	}
 	return nil
 }
