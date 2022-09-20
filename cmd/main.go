7b28b413aaf1096b2504d19a75d80c8ab892301e
CodeXzzz <codexzzz@outlook.com>
2022-09-20 20:50:09 +0800

    fix:rabbitmq leads to cannot access the port

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"SummerProject/common"
 	"SummerProject/config"
-	"SummerProject/internal/middleware/rabbitmq"
 	"SummerProject/internal/router"
 )
 
@@ -20,9 +19,9 @@ func main() {
 	//雪花算法优化版
 	common.InitID()
 
-	//启动rabbitmq服务
-	rabbitmq.SendInit()
-	rabbitmq.ReceiveInit()
+	//启动rabbitmq
+	//rabbitmq.SendInit()
+	//rabbitmq.ReceiveInit()
 
 	//注册路由
 	router.InitRouter()
